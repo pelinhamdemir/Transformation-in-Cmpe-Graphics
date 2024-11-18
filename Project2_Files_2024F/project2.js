945885404be737b1c2de485a77d1eeca49086e32
pelinhamdemir <160609031+pelinhamdemir@users.noreply.github.com>
2024-11-18 12:36:22 +0300

    first commit

---
+++ Project2_Files_2024F/project2.js
@@ -0,0 +1,43 @@
+// TO DO 1: Provides a 3x3 transformation matrix represented as an array containing 9 values arranged in column-major sequence.
+// Initially, the transformation employs scaling, followed by rotation, and ultimately, translation.
+// The specified rotation measurement is in degrees.
+function GetTransform(positionX, positionY, rotation, scale) {
+
+}
+
+// TO DO 2:Provides a 3x3 transformation matrix represented as an array containing 9 values arranged in column-major sequence.
+// The inputs consist of transformation matrices following the identical format.
+// The resulting transformation initially employs trans1 and subsequently applies trans2.
+function ApplyTransform(trans1, trans2) {
+
+}
+
+
+//You will write the function for ResetTransform
+//This function resets the drone’s position, rotation, scale, and altitude
+function resetTransform() {
+
+
+}
+
+
+//You will write the function for boostSpeed
+//Temporarily increases the drone's speed for 5 seconds and then resets it to the normal speed.
+function boostSpeed() {
+
+
+}
+
+//You will write the function for hoverMode
+//Increases the altitude to 50 units and stops the drone from moving horizontally.
+function hoverMode() {
+
+
+}
+
+//You will write the function for  mouseMovement
+//The drone follows the mouse unless it is in hover modes
+function mouseMovement() {
+
+
+}
